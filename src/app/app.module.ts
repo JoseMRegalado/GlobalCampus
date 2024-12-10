6daa09751225da0369005037dc1426bc7ebcbcb7
JoseMRegalado <mjregalado2@utpl.edu.ec>
2024-12-10 12:06:58 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    RouterOutlet,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA6G63WR0gjZ8_AO1Ot_TBLqWnmYKbmZ7U",
+    authDomain: "globalcampus-1a1d9.firebaseapp.com",
+    projectId: "globalcampus-1a1d9",
+    storageBucket: "globalcampus-1a1d9.firebasestorage.app",
+    messagingSenderId: "35671681088",
+    appId: "1:35671681088:web:b1f6b973bb0b467dbcd7a5"
+  }
+};
